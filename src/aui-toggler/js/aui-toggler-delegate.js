163bcd4d070979ac23d40c7e23111d375077420e
Thiago Rocha <thiago.rocha@liferay.com>
2014-03-26 17:54:41 -0300

    AUI-1196 Update `createAll` function

--- src/aui-toggler/js/aui-toggler-delegate.js
+++ src/aui-toggler/js/aui-toggler-delegate.js
@@ -237,9 +237,16 @@ var TogglerDelegate = A.Component.create({
          */
         createAll: function() {
             var instance = this;
-
+ 
             instance.get('container').all(instance.get('header')).each(function(header) {
-                if (!header.getData('toggler')) {
+                var markupCollapsed = header.hasClass(CSS_TOGGLER_HEADER_COLLAPSED),
+                    markupExpanded = header.hasClass(CSS_TOGGLER_HEADER_EXPANDED),
+                    expanded = instance.get('expanded'),
+                    closeAllOnExpand = instance.get('closeAllOnExpand');
+ 
+                if ((closeAllOnExpand && markupExpanded) ||
+                    (expanded && markupCollapsed) ||
+                    (!expanded && !markupExpanded && !markupCollapsed) && !header.getData('toggler')) {
                     instance._create(header);
                 }
             });
