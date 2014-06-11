896ed2891610e2364ea5b5d9c0468ad5ecbd6e33
Henrique Vicente <henriquevicente@gmail.com>
2014-06-11 15:14:03 -0300

    AUI-1328 Increase the low number of file descriptors for running tasks on OS X
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var requireDir = require('require-dir');
 var tasksDir = requireDir('./tasks');
 
+// Increase Darwin's (OS X) low (256) file descriptors default soft limit
+require('osx-ulimit').set(500000);
+
 // Add the node_modules/.bin directory to the PATH
 require('spawn-local-bin').path(__dirname);
